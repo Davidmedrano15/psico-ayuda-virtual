d8ea4ced698f021f7f9bcb3da79d880bd738360d
David Alejandro Medrano <102700809+Davidmedrano15@users.noreply.github.com>
2023-10-22 17:06:03 -0500

    psico-ayuda-virtual

--- src/componentes/nuestro-trabajo.js
+++ src/componentes/nuestro-trabajo.js
@@ -2,7 +2,6 @@ import React from "react";
 import "../css/nuestro-trabajo.css";
 
 function Servicios() {
-    
   return (
     <div id="Servicios" className="servi">
       {/* Contenido de la sección de Servicios */}
@@ -19,8 +18,10 @@ function Servicios() {
           <li>
             <h3 className="servi__t">Privacidad</h3>
             <p className="servi-p">
-              Contamos con la experiencia que guardarán la confidencialidad de
-              la información, además iremos a tu tiempo, si requieres tener la
+              Contamos con la experiencia que guardarán la confidencialidad de{" "}
+              <br />
+              la información, además iremos a tu tiempo, si requieres tener la{" "}
+              <br />
               cámara apagada mientras tomas algo de confianza, es válido.
             </p>
           </li>
@@ -29,6 +30,7 @@ function Servicios() {
             <h3 className="servi__t">Comodidad</h3>
             <p className="servi-p">
               Reside tu atención psicológica en línea, en cualquier lugar y
+              <br />
               cuando lo desees.
             </p>
           </li>
@@ -37,7 +39,9 @@ function Servicios() {
             <h3 className="servi__t">Ahorro</h3>
             <p className="servi-p">
               Ahorro de tiempo, ya que no necesitarás desplazarte, evitar el
+              <br />
               tráfico, mal clima, por ello ahorras tiempo y dinero que en una
+              <br />
               cita presencial.
             </p>
           </li>
@@ -46,8 +50,11 @@ function Servicios() {
             <h3 className="servi__t">Comprensión</h3>
             <p className="servi-p">
               Sabemos que estar lejos de tu país es difícil pues te enfrentas a
+              <br />
               una cultura y rituales desconocidos, por ello es fundamental tener
+              <br />
               un profesional que es de tu misma región, ello garantiza conocer
+              <br />
               tu cultura, y los ritos que cada uno de ellos tienen.
             </p>
           </li>
